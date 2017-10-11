8586034bc946dd4a0dd000ab42fb75c2368fbd5f
Anil <anil_mndl@yahoo.com>
2017-10-10 20:12:01 -0600

    initial commit

---
+++ more-methods/more-methods/more-methods/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace more_methods
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButtonBus = new System.Windows.Forms.RadioButton();
+            this.radioBtnBicycle = new System.Windows.Forms.RadioButton();
+            this.trackBar = new System.Windows.Forms.TrackBar();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(100, 227);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioBtnBicycle);
+            this.groupBox1.Controls.Add(this.radioButtonBus);
+            this.groupBox1.Location = new System.Drawing.Point(43, 47);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Travel";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // radioButtonBus
+            // 
+            this.radioButtonBus.AutoSize = true;
+            this.radioButtonBus.Location = new System.Drawing.Point(36, 19);
+            this.radioButtonBus.Name = "radioButtonBus";
+            this.radioButtonBus.Size = new System.Drawing.Size(43, 17);
+            this.radioButtonBus.TabIndex = 0;
+            this.radioButtonBus.TabStop = true;
+            this.radioButtonBus.Text = "Bus";
+            this.radioButtonBus.UseVisualStyleBackColor = true;
+            // 
+            // radioBtnBicycle
+            // 
+            this.radioBtnBicycle.AutoSize = true;
+            this.radioBtnBicycle.Location = new System.Drawing.Point(36, 42);
+            this.radioBtnBicycle.Name = "radioBtnBicycle";
+            this.radioBtnBicycle.Size = new System.Drawing.Size(59, 17);
+            this.radioBtnBicycle.TabIndex = 1;
+            this.radioBtnBicycle.TabStop = true;
+            this.radioBtnBicycle.Text = "Bicycle";
+            this.radioBtnBicycle.UseVisualStyleBackColor = true;
+            // 
+            // trackBar
+            // 
+            this.trackBar.Location = new System.Drawing.Point(43, 153);
+            this.trackBar.Name = "trackBar";
+            this.trackBar.Size = new System.Drawing.Size(177, 45);
+            this.trackBar.TabIndex = 2;
+            this.trackBar.Scroll += new System.EventHandler(this.trackBar_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.trackBar);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioBtnBicycle;
+        private System.Windows.Forms.RadioButton radioButtonBus;
+        private System.Windows.Forms.TrackBar trackBar;
+    }
+}
+
---
+++ more-methods/more-methods/more-methods/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace more_methods
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show(GetColumnName().ToString());
+            // MessageBox.Show(GetColumnName().ToString());
+
+            //MessageBox.Show(GetColumnName(6).ToString());
+
+            //string myName = "Idaho State University";
+            //Console.WriteLine(myName);
+
+            //string someName = PassByValue(myName);
+            //PassByRef(out myName);
+            //Console.WriteLine(someName);
+
+            //Console.WriteLine(myName);
+
+            // PassByValue
+            //Idaho State University = myName
+            //Some other name = someName
+            //Idaho State University = myName
+
+            // Idaho State University
+            // Some other name
+
+            if (radioButtonBus.Checked)
+            {
+                MessageBox.Show("You clicked on bus");
+            }
+            else if(radioBtnBicycle.Checked)
+            {
+                MessageBox.Show("You clicked on bicyle");
+            }
+            else
+            {
+                MessageBox.Show("You didnt clicked on anything");
+            }
+
+            MessageBox.Show(getTipAmt(25).ToString());
+
+        }
+
+        private string PassByValue(string name)
+        {
+            name = "Some other name";
+            return name;
+        }
+
+        private string PassByRef(out string name)
+        {
+            name = "Some other name";
+            return name;
+        }
+
+
+        private int getcolumnname()
+        {
+            return 5;
+        }
+
+        private int GetColumnName(int num1 = 3)
+        {
+            return num1;
+        }
+
+        private int GetColumnName(int num0, int num1 = 5)
+        {
+            return num0 + num1;
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackBar_Scroll(object sender, EventArgs e)
+        {
+            //MessageBox.Show(trackBar.Value.ToString());
+        }
+
+        private double getTipAmt(double billAmt)
+        {
+            double tipPercent = trackBar.Value;
+            return billAmt * (tipPercent / 100); 
+        }
+    }
+}
